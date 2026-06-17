923797aa9e37033786326fa9aa34dabca4949f9b
accostantin <anaclaracmorel@gmail.com>
2026-06-17 12:21:48 -0300

    atual
    a

---
+++ react_01/src/header.jsx
@@ -0,0 +1,19 @@
+
+function Header() {
+    return (
+      <header className="header">
+        <div className="logo">
+        <h1 className="titulo">VEXA</h1>
+            </div>
+        <nav>
+          <ul>
+            <li><a href="/">Início</a></li>
+            <li><a href="/sobre">Sobre</a></li>
+            <li><a href="/contato">Contato</a></li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+  
+  export default Header;
