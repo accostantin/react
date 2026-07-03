e68f68b177e20b6271e33a24711a75cc7543233b
accostantin <anaclaracmorel@gmail.com>
2026-07-03 09:22:36 -0300

    .

--- react_01/src/App.jsx
+++ react_01/src/App.jsx
@@ -1,12 +1,14 @@
 import Header from './header.jsx';
 import Button from './btn.jsx';
 import Carrossel from './Carrossel.jsx';
+
 import './App.css';
 
 function App() {
   return (
     <div>
       <Header/> 
+      
       <Carrossel/>
     </div>
   );
--- react_01/src/Carrossel.jsx
+++ react_01/src/Carrossel.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react';
 import './Carrossel.css';
 
-function Carrossel() {
-  const imagens = [
-    '/banner1.jfif',
-    '/banner2.jfif',
-    '/banner3.jfif',
+function Carrossel() { 
+  const imagens = [ 
+    './src/assets/marie.png',
+    './src/assets/2.png',
+    './src/assets/3.png',
   ];
 
   const [indice, setIndice] = useState(0);
@@ -21,17 +21,17 @@ function Carrossel() {
   return (
     <main>
       <div className="carrossel">
+      <button className="botaoesquerda" onClick={anterior}>
+          ◀
+        </button>
         <img
           src={imagens[indice]}
           alt={`Slide ${indice + 1}`}
           className="imagem"
         />
 
-        <button className="botao esquerda" onClick={anterior}>
-          ◀
-        </button>
 
-        <button className="botao direita" onClick={proximo}>
+        <button className="botaodireita" onClick={proximo}>
           ▶
         </button>
       </div>
