f9419abc3bb7de42ae3ac70d628cd7a2cd3be74b
accostantin <anaclaracmorel@gmail.com>
2026-08-05 10:03:08 -0300

    a
    a

---
+++ react_01/src/components/banner.jsx
@@ -0,0 +1,29 @@
+import "./banner.css";
+import marie from "./assets/marie.png";
+
+function Banner() {
+  return (
+    <main className="banner-main">
+      <div className="banner-container">
+
+        <img
+           src={marie}
+           alt="Banner principal"
+           className="banner-img"
+        />
+
+        <div className="banner-content">
+          <p>
+            Descubra novos conteúdos e participe da comunidade.
+          </p>
+
+          <button className="banner-btn"> 
+          </button>
+        </div>
+
+      </div>
+    </main>
+  );
+}
+
+export default Banner;
